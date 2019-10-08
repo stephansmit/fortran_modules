11efb6b3f9c488a35977ecf7ce1a75a2efbc8112
stephan <stephansmit@hotmail.com>
2019-10-08 16:54:32 +0200

    cleaned up:

--- turbmodels.f90
+++ turbmodels.f90
@@ -13,30 +13,7 @@ module TurbModels
     end function getValue
   end interface
 
-
-  
-
-
 end module TurbModels
 
 
 
-module Test
-use TurbModels
-implicit none
-public 
-  class(AbstractTurbModel), allocatable :: m
-
-contains
-  subroutine init(model)
-    use TurbModels_SST
-    use TurbModels_MK
-    implicit none
-    integer model
-    if (model .eq. 1) then
-      allocate(SSTTurbModel::m)
-    else
-      allocate(MKTurbModel::m)      
-    endif
-  end subroutine init
-end module Test
