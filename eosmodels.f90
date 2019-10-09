db07624495575f35d1560dae9b3d45ff009e963c
stephan <stephansmit@hotmail.com>
2019-10-09 10:45:42 +0200

    latest

--- eosmodels.f90
+++ eosmodels.f90
@@ -39,82 +39,108 @@ contains
 end module IG_EOSModels
 
 
-
 module Table_EOSModels
 use EOSModels
 implicit none
 
-
-  type, extends(EOSModel), public :: Table_EOSModel
-  real(8), dimension(:), allocatable :: tempTab,rhoTab,betaTab, muTab,lamTab, cpTab,enthTab,lamocpTab, &
-                                        temp2Tab,rho2Tab,beta2Tab, mu2Tab,lam2Tab, cp2Tab,enth2Tab,lamocp2Tab
-  integer, public :: nTab 
-  Character(len =30), public :: tablefile
+  type, extends(EOSModel), public :: Table_EOSModel 
   contains 
       procedure :: init
   end type Table_EOSModel
 
 contains  
-
-  subroutine init(this)
-
+  subroutine init(this) 
     class(Table_EOSModel) :: this
-    call set_constants(this)
-    call allocateMem(this)
-    call readtable(this)
-    call calc_int_coeff(this)
-  end subroutine init
-
-
-  subroutine set_constants(this)
-    class(Table_EOSModel) :: this
-    this%nTab = 2000
-    this%tablefile = 'co2h_table.dat'
-  end subroutine set_constants
-
-
-  subroutine allocateMem(this) 
-    class(Table_EOSModel) :: this
-    integer i
-    allocate(this%tempTab(1:this%nTab),this%rhoTab(1:this%nTab),this%betaTab(1:this%nTab), &
-             this%muTab(1:this%nTab),this%lamTab(1:this%nTab),this%cpTab(1:this%nTab), &
-             this%enthTab(1:this%nTab),this%lamocpTab(1:this%nTab),this%temp2Tab(1:this%nTab), &
-             this%rho2Tab(1:this%nTab),this%beta2Tab(1:this%nTab),this%mu2Tab(1:this%nTab), &
-             this%lam2Tab(1:this%nTab),this%cp2Tab(1:this%nTab),this%enth2Tab(1:this%nTab), &
-             this%lamocp2Tab(1:this%nTab))
+    this%A = 6.0
   end subroutine
-
-  
-  subroutine readtable(this)
-    integer i
-    class(Table_EOSModel) :: this
-    open(27,file=this%tablefile)
-    do i=1,this%nTab
-      read (27,*) this%tempTab(i),this%rhoTab(i),this%muTab(i),this%lamTab(i), &
-                  this%cpTab(i),this%enthTab(i),this%betaTab(i)
-      this%lamocpTab(i) = this%lamTab(i)/this%cpTab(i)
-    enddo
-    close(27)
-  end subroutine readtable
-
-
-  subroutine calc_int_coeff(this)
-    use mod_math
-    class(Table_EOSModel) :: this
-    call spline(this%tempTab,this%enthTab,  this%nTab,this%enth2Tab)
-    call spline(this%enthTab,this%rhoTab,   this%nTab,this%rho2Tab)
-    call spline(this%enthTab,this%muTab,    this%nTab,this%mu2Tab)
-    call spline(this%enthTab,this%lamTab,   this%nTab,this%lam2Tab)
-    call spline(this%enthTab,this%cpTab,    this%nTab,this%cp2Tab)
-    call spline(this%enthTab,this%lamocpTab,this%nTab,this%lamocp2Tab)
-    call spline(this%enthTab,this%tempTab,  this%nTab,this%temp2Tab)
-    call spline(this%enthTab,this%betaTab,  this%nTab,this%beta2Tab)
-  end subroutine calc_int_coeff
-
-
 
 
 end module Table_EOSModels
+
+
+
+
+
+! module Table_EOSModels
+! use EOSModels
+! implicit none
+
+
+!   type, extends(EOSModel), public :: Table_EOSModel
+!   real(8), dimension(:), allocatable :: tempTab,rhoTab,betaTab, muTab,lamTab, cpTab,enthTab,lamocpTab, &
+!                                         temp2Tab,rho2Tab,beta2Tab, mu2Tab,lam2Tab, cp2Tab,enth2Tab,lamocp2Tab
+!   integer, public :: nTab 
+!   Character(len =30), public :: tablefile
+!   contains 
+!       procedure :: init
+!   end type Table_EOSModel
+
+! contains  
+
+!   subroutine init(this)
+!     implicit none
+!     class(Table_EOSModel) :: this
+!     call allocateMem(this)
+!     call readtable(this)
+!     call calc_int_coeff(this)
+!   end subroutine init
+
+
+!   subroutine set_table(this, filename, ntab)
+!     implicit none
+!     class(Table_EOSModel) :: this
+!     integer nTab
+!     Character(len=30) filename
+!     this%nTab = ntab
+!     this%tablefile = 'co2h_table.dat'
+!   end subroutine set_table
+
+
+!   subroutine allocateMem(this) 
+!     implicit none
+!     class(Table_EOSModel) :: this
+!     integer i
+!     allocate(this%tempTab(1:this%nTab),this%rhoTab(1:this%nTab),this%betaTab(1:this%nTab), &
+!              this%muTab(1:this%nTab),this%lamTab(1:this%nTab),this%cpTab(1:this%nTab), &
+!              this%enthTab(1:this%nTab),this%lamocpTab(1:this%nTab),this%temp2Tab(1:this%nTab), &
+!              this%rho2Tab(1:this%nTab),this%beta2Tab(1:this%nTab),this%mu2Tab(1:this%nTab), &
+!              this%lam2Tab(1:this%nTab),this%cp2Tab(1:this%nTab),this%enth2Tab(1:this%nTab), &
+!              this%lamocp2Tab(1:this%nTab))
+!   end subroutine
+
+  
+!   subroutine readtable(this)
+!     implicit none
+!     integer i
+!     class(Table_EOSModel) :: this
+!     open(27,file=this%tablefile)
+!     do i=1,this%nTab
+!       read (27,*) this%tempTab(i),this%rhoTab(i),this%muTab(i),this%lamTab(i), &
+!                   this%cpTab(i),this%enthTab(i),this%betaTab(i)
+!       this%lamocpTab(i) = this%lamTab(i)/this%cpTab(i)
+!     enddo
+!     close(27)
+!   end subroutine readtable
+
+
+!   subroutine calc_int_coeff(this)
+!     use mod_math
+!     implicit none
+!     class(Table_EOSModel) :: this
+!     call spline(this%tempTab,this%enthTab,  this%nTab,this%enth2Tab)
+!     call spline(this%enthTab,this%rhoTab,   this%nTab,this%rho2Tab)
+!     call spline(this%enthTab,this%muTab,    this%nTab,this%mu2Tab)
+!     call spline(this%enthTab,this%lamTab,   this%nTab,this%lam2Tab)
+!     call spline(this%enthTab,this%cpTab,    this%nTab,this%cp2Tab)
+!     call spline(this%enthTab,this%lamocpTab,this%nTab,this%lamocp2Tab)
+!     call spline(this%enthTab,this%tempTab,  this%nTab,this%temp2Tab)
+!     call spline(this%enthTab,this%betaTab,  this%nTab,this%beta2Tab)
+!   end subroutine calc_int_coeff
+
+
+
+
+! end module Table_EOSModels
 
 
 ! module CO2Table_EOSModels
--- models.f90
+++ models.f90
@@ -10,19 +10,21 @@ contains
   subroutine initEOSModel(model)
     ! use H2Table_EOSModels
     use Table_EOSModels
-
     use IG_EOSModels
 
     implicit none
     integer model
     if (model .eq. 1) then
       allocate(IG_EOSModel::eosm)
+      call eosm%init()
+
     ! else if (model .eq. 2) then
     !   allocate(CO2Table_EOSModel::eosm)      
     else
-      allocate(Table_EOSModel::eosm)      
+      allocate(Table_EOSModel::eosm)   
+      ! call eosm%set_table('co2h_table.dat', 2000) 
+      call eosm%init()
     endif
-    call eosm%init()
   end subroutine initEOSModel
 
 end module Models
