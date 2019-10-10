3ddb65d4b9542a5df5a63ab3f5ab6a82b8884d01
stephan <stephansmit@hotmail.com>
2019-10-10 11:50:41 +0200

    final version

--- main.f90
+++ main.f90
@@ -3,10 +3,9 @@ program modular
   implicit none
   include 'mpif.h'
   integer ploc,ierr, rank
-  real(8) Re, Pr, output, value
+  real(8) Re, Pr, output_ig,output_table, value
   class(EOSModel), pointer :: eosmodel_ig
   class(EOSModel), pointer :: eosmodel_table
-
 
   call mpi_init(ierr)
   call mpi_comm_rank(MPI_COMM_WORLD,rank,ierr)
@@ -14,34 +13,19 @@ program modular
 
   Re = 200
   Pr = 200
-  
+  value = 0.01  
+
   allocate(eosmodel_ig,    source=IG_EOSModel(Re,Pr))
   allocate(eosmodel_table, source=Table_EOSModel(Re,Pr,2000,"co2h_table.dat"))
-
 
   call eosmodel_ig%init()
   call eosmodel_table%init()
   
+  call eosmodel_ig%   set(value,'L', output_ig)
+  call eosmodel_table%set(value,'L', output_table)
 
-  value = 0.01
-  call eosmodel_ig%set(value,'L', output)
-  write(*,*) output
-  call eosmodel_table%set(value,'L', output)
-  write(*,*) output
+  write(*,*) output_ig, output_table
 
   call mpi_finalize(ierr)
 
 end program
-
-
-
-
-! subroutine testbase(input)
-!   use mod_types
-!   implicit none
-!   type(TDerivedClass) :: input
-!   write(*,*) input%in2
-
-
-
-! end subroutine testbase
