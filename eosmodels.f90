ee68be823c6163e200f1c8f0b374be7261a64e73
stephan <stephansmit@hotmail.com>
2019-10-16 14:57:23 +0200

    added

--- eosmodels.f90
+++ eosmodels.f90
@@ -1,4 +1,4 @@
-module mod_eosmodels
+module mod_classes
   implicit none
 
 
@@ -6,173 +6,93 @@ module mod_eosmodels
   !     Abstract class     !
   !!!!!!!!!!!!!!!!!!!!!!!!!!
   
-  type, abstract, public :: EOSModel
-  real(8) Re, Pr
+  type, abstract, public :: BaseClass
+  real(8) :: Re, Pr
   contains
-    procedure(init), deferred :: init
-    procedure(set), deferred :: set
-  end type EOSModel
+    procedure(init_bc), deferred :: init
+    procedure(set_bc), deferred :: set
+  end type BaseClass
 
   interface
-    subroutine init(this)
-      import :: EOSModel
-      class(EOSModel) :: this
-    end subroutine init
-    subroutine set( this, enth, prop, output )
-      import :: EOSModel
-      class(EOSModel) :: this
-      real(8), intent(IN) :: enth
-      character(len=1), intent(IN) :: prop
+    subroutine init_bc(this)
+      import :: BaseClass
+      class(BaseClass) :: this
+    end subroutine init_bc
+    subroutine set_bc( this, string,output)
+      import :: BaseClass
+      class(BaseClass) :: this
+      character(len=1), intent(IN) :: string
       real(8), intent(OUT) :: output
-    end subroutine set
+    end subroutine set_bc
   end interface
 
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!
-  !     Ideal Gas class    !
+  ! Second Abstract class  ! 
   !!!!!!!!!!!!!!!!!!!!!!!!!!
   
-  type, extends(EOSModel), public :: IG_EOSModel
+  type, abstract, extends(BaseClass), public :: SecondBaseClass
   contains
-    procedure :: init => initialize_ig
-    procedure :: set => set_w_ig
-  end type IG_EOSModel
+    procedure(init_sbc), deferred :: init
+    procedure(set_sbc), deferred :: set
+  end type SecondBaseClass
+
+  interface
+    subroutine init_sbc(this)
+      import :: SecondBaseClass
+      class(SecondBaseClass) :: this
+    end subroutine init_sbc
+    subroutine set_sbc( this, string,output)
+      import :: SecondBaseClass
+      class(SecondBaseClass) :: this
+      character(len=1), intent(IN) :: string
+      real(8), intent(OUT) :: output
+    end subroutine set_sbc
+  end interface
+
 
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!
   !       Table class      !
   !!!!!!!!!!!!!!!!!!!!!!!!!!
   
-  type, extends(EOSModel), public :: Table_EOSModel
-  integer ntab
-  character(len=20) filename
-  real(8), dimension(:), private, allocatable :: tempTab,rhoTab,betaTab, muTab,lamTab, &
-                                                 cpTab,enthTab,lamocpTab, temp2Tab,    &
-                                                 rho2Tab,beta2Tab, mu2Tab,lam2Tab,     &
-                                                 cp2Tab,enth2Tab,lamocp2Tab
+  type, extends(SecondBaseClass), public :: Implemented
   contains
-    procedure :: init => initialize_table
-    procedure :: set => set_w_table
-    procedure, private :: allocate_mem => allocate_mem
-    procedure, private :: read_table => read_table
-    procedure, private :: calc_interp_coeff => calc_interp_coeff
-  end type Table_EOSModel
+    procedure :: init => initialize_imp
+    procedure :: set => set_w_imp
+   end type Implemented
 
 
 
 contains
+  ! type(SecondBaseClass) function init_SecondBaseClass(Re,Pr)
+  !   real(8), intent(in) :: Re, Pr
+  !   init_SecondBaseClass%Re = Re
+  !   init_SecondBaseClass%Pr = Pr
+  ! end function init_SecondBaseClass
+
+  type(Implemented) function init_Implemented(Re,Pr)
+    real(8), intent(in) :: Re, Pr
+    init_Implemented%Re = Re
+    init_Implemented%Pr = Pr
+  end function init_Implemented
 
   !!!!!!!!!!!!!!!!!!!!!!!!!!
   !   Ideal Gas routines   !
   !!!!!!!!!!!!!!!!!!!!!!!!!!
 
-  subroutine initialize_ig(this)
+  subroutine initialize_imp(this)
     implicit none
-    class(IG_EOSModel) :: this
-  end subroutine initialize_ig
+    class(Implemented) :: this
+  end subroutine initialize_imp
 
-  subroutine set_w_ig(this, enth, prop, output)
+  subroutine set_w_imp(this, string, output)
     implicit none
-    class(IG_EOSModel) :: this
-    real(8), intent(IN) :: enth
-    character(len=1), intent(IN) :: prop
+    class(Implemented) :: this
+    character(len=1), intent(IN) :: string
     real(8), intent(OUT) :: output
-    select case (prop)
-      case ("T")
-        output = enth + 1.0
-      case ("D")
-        output = 1/(enth+1)
-      case ("L")
-        output = 1./(this%Re*this%Pr)
-      case ("C")
-        output = 1.
-      case ("V")
-        output = 1./this%Re
-      case ("B")
-        output = 1./(enth+1)
-      case default
-        write(*,*) "Property doesn't exist!!!"
-    end select
-  end subroutine set_w_ig
+    output = 4
+  end subroutine set_w_imp
 
 
-
-  !!!!!!!!!!!!!!!!!!!!!!!!!!
-  !     Table routines     !
-  !!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  subroutine initialize_table(this)
-    implicit none
-    class(Table_EOSModel) :: this
-    call this%allocate_mem()
-    call this%read_table()
-    call this%calc_interp_coeff()
-  end subroutine initialize_table
-
-  subroutine set_w_table(this, enth, prop, output)
-    use mod_math
-    implicit none
-    class(Table_EOSModel) :: this
-    real(8), intent(IN) :: enth
-    character(len=1), intent(IN) :: prop
-    real(8), intent(OUT):: output
-    integer :: tabkhi,tabklo = 0 
-    select case (prop)
-      case ("T")
-        call splint(this%enthTab,this%tempTab,  this%temp2Tab,  this%nTab,enth,output,tabkhi,tabklo)
-      case ("D")
-        call splint(this%enthTab,this%rhoTab,   this%rho2Tab,   this%nTab,enth,output,tabkhi,tabklo)
-      case ("L")
-        call splint(this%enthTab,this%lamocpTab,this%lamocp2Tab,this%nTab,enth,output,tabkhi,tabklo)
-        output = output/(this%Re*this%Pr)
-      case ("C")
-        call splint(this%enthTab,this%cpTab,    this%cp2Tab,    this%nTab,enth,output,tabkhi,tabklo)
-      case ("V")
-        call splint(this%enthTab,this%muTab,    this%mu2Tab,    this%nTab,enth,output,tabkhi,tabklo)
-        output = output/this%Re
-      case ("B")
-        call splint(this%enthTab,this%betaTab,  this%beta2Tab,  this%nTab,enth,output,tabkhi,tabklo)
-      case default
-        write(*,*) "Property doesn't exist!!!"
-    end select
-  end subroutine set_w_table
-
-  subroutine allocate_mem(this) 
-    implicit none
-    class(Table_EOSModel) :: this
-    allocate(this%tempTab(1:this%nTab),this%rhoTab(1:this%nTab),   this%betaTab(1:this%nTab),  &
-             this%muTab(1:this%nTab),  this%lamTab(1:this%nTab),   this%cpTab(1:this%nTab),    &
-             this%enthTab(1:this%nTab),this%lamocpTab(1:this%nTab),this%temp2Tab(1:this%nTab), &
-             this%rho2Tab(1:this%nTab),this%beta2Tab(1:this%nTab), this%mu2Tab(1:this%nTab),   &
-             this%lam2Tab(1:this%nTab),this%cp2Tab(1:this%nTab),   this%enth2Tab(1:this%nTab), &
-             this%lamocp2Tab(1:this%nTab))
-  end subroutine allocate_mem
-
-  subroutine read_table(this)
-    implicit none
-    class(Table_EOSModel) :: this
-    integer i, ierr
-    open(27,file=this%filename)
-    do i=1,this%nTab
-      read (27,*) this%tempTab(i),this%rhoTab(i), this%muTab(i),this%lamTab(i), &
-                  this%cpTab(i),  this%enthTab(i),this%betaTab(i)
-      this%lamocpTab(i) = this%lamTab(i)/this%cpTab(i)
-    enddo
-    close(27)
-  end subroutine read_table
-
-  subroutine calc_interp_coeff(this)
-    use mod_math
-    implicit none
-    class(Table_EOSModel) :: this
-    call spline(this%tempTab,this%enthTab,  this%nTab,this%enth2Tab)
-    call spline(this%enthTab,this%rhoTab,   this%nTab,this%rho2Tab)
-    call spline(this%enthTab,this%muTab,    this%nTab,this%mu2Tab)
-    call spline(this%enthTab,this%lamTab,   this%nTab,this%lam2Tab)
-    call spline(this%enthTab,this%cpTab,    this%nTab,this%cp2Tab)
-    call spline(this%enthTab,this%lamocpTab,this%nTab,this%lamocp2Tab)
-    call spline(this%enthTab,this%tempTab,  this%nTab,this%temp2Tab)
-    call spline(this%enthTab,this%betaTab,  this%nTab,this%beta2Tab)
-  end subroutine calc_interp_coeff
-
-end module mod_eosmodels
+end module mod_classes
--- main.f90
+++ main.f90
@@ -1,30 +1,27 @@
 program modular
-  use mod_eosmodels
+  use mod_classes
   implicit none
   include 'mpif.h'
   integer ploc,ierr, rank
   real(8) Re, Pr, output_ig,output_table, value
-  class(EOSModel), pointer :: eosmodel_ig
-  class(EOSModel), pointer :: eosmodel_table
-
+  class(BaseClass), allocatable :: eosmodel_ig
+  
   call mpi_init(ierr)
   call mpi_comm_rank(MPI_COMM_WORLD,rank,ierr)
   call mpi_comm_size(MPI_COMM_WORLD,ploc,ierr)
 
   Re = 200
   Pr = 200
-  value = 0.01  
+  ! value = 0.01  
 
-  allocate(eosmodel_ig,    source=IG_EOSModel(Re,Pr))
-  allocate(eosmodel_table, source=Table_EOSModel(Re,Pr,2000,"co2h_table.dat"))
+  allocate(eosmodel_ig,    source=init_Implemented(Re,Pr))
+  ! allocate(eosmodel_table, source=Table_EOSModel(Re,Pr,2000,"co2h_table.dat"))
 
   call eosmodel_ig%init()
-  call eosmodel_table%init()
   
-  call eosmodel_ig%   set(value,'L', output_ig)
-  call eosmodel_table%set(value,'L', output_table)
-
-  write(*,*) output_ig, output_table
+  call eosmodel_ig%   set('L', output_ig)
+  
+  write(*,*) output_ig
 
   call mpi_finalize(ierr)
 
